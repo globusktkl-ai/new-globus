6f96963c4f8a7b3b55abce9213cea59d621b3fa1
globusktkl-ai <globusktkl@gmail.com>
2026-07-30 21:24:05 +0530

    Add files via upload

---
+++ assets/js/supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL='https://kwrugdbrzrfbmibaccwr.supabase.co';
+const SUPABASE_ANON_KEY='sb_publishable_Pf_pB13Hv4ycYmNSiD75XQ_cT0b5eOM';
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
