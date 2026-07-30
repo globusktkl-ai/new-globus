d8cc0982f18b21dbacfa4febc14994839987febb
globusktkl-ai <globusktkl@gmail.com>
2026-07-30 20:52:15 +0530

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,7 +1,8 @@
 const SUPABASE_URL = 'https://kwrugdbrzrfbmibaccwr.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_Pf_pB13Hv4ycYmNSiD75XQ_cT0b5eOM';
 
-window.supabaseClient = window.supabase.createClient(
+// Create client immediately
+window.supabaseClient = supabase.createClient(
 SUPABASE_URL,
 SUPABASE_ANON_KEY
 );
