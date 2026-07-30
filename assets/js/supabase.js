0cd60c1cfba62b15d11b5fd8309e6c2a9f816748
globusktkl-ai <globusktkl@gmail.com>
2026-07-30 21:21:01 +0530

    Delete assets/js/supabase.js

--- assets/js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = 'https://kwrugdbrzrfbmibaccwr.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_Pf_pB13Hv4ycYmNSiD75XQ_cT0b5eOM';
-
-// Create client immediately
-window.supabaseClient = supabase.createClient(
-SUPABASE_URL,
-SUPABASE_ANON_KEY
-);
